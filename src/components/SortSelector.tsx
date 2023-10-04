1d8f78755f8a968b1ad40e4c5d0db5e6e81b5dda
NABIA <nabia.khan@cloudways.com>
2023-10-04 16:10:09 +0500

    sorted the games

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,19 +1,40 @@
 import { Menu, MenuButton, Button, MenuList, MenuItem } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
-const SortSelector = () => {
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  selectedSortOrder: string;
+}
+
+const SortSelector = ({ onSelectSortOrder, selectedSortOrder }: Props) => {
+  const sortOrders = [
+    { label: "Relevance", value: "" }, //default sort order -> value is left empty
+    { label: "Date Added", value: "-added" }, //hyphen for reverse order coz I want the latest added on top
+    { label: "Name", value: "name" },
+    { label: "Release Date", value: "-released" },
+    { label: "Popularity", value: "-metacritic" }, //highest rating on top
+    { label: "Average Rating", value: "-rating" },
+  ];
+
+  const currentSortOrder = sortOrders.find(
+    (order) => order.value === selectedSortOrder
+  );
+
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        Order by: Relevance
+        Order by: {currentSortOrder?.label || "Relevance"}
       </MenuButton>
       <MenuList>
-        <MenuItem>Relevance</MenuItem>
-        <MenuItem>Date Added</MenuItem>
-        <MenuItem>Name</MenuItem>
-        <MenuItem>Release Date</MenuItem>
-        <MenuItem>Popularity</MenuItem>
-        <MenuItem>Average Rating</MenuItem>
+        {sortOrders.map(({ label, value }) => (
+          <MenuItem
+            key={value}
+            value={value}
+            onClick={() => onSelectSortOrder(value)}
+          >
+            {label}
+          </MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
