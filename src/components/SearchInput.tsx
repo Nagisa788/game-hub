7b2ace046602ab99f05ab568aa28569ef1df5f14
NABIA <nabia.khan@cloudways.com>
2023-10-09 17:43:12 +0500

    built the search input component

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,9 @@
+import { Input } from "@chakra-ui/react";
+
+const SearchInput = () => {
+  return (
+    <Input borderRadius={20} variant="filled" placeholder="Search Games..." />
+  );
+};
+
+export default SearchInput;
