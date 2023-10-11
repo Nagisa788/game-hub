33b5506eeafd1c5e3b731ea4ebf9e0eafda1057e
NABIA <nabia.khan@cloudways.com>
2023-10-11 18:04:38 +0500

    updated page title

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
 import theme from "./theme";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
